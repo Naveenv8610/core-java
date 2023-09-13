542867e7797e47c99a032b7f8a5fe2477fd524a9
Naveenv8610 <naveenv.xworkz@gmail.com>
2023-09-13 09:36:28 +0530

    uploading method over loading

---
+++ Flipkart2.java
@@ -0,0 +1,155 @@
+class Flipkart2 {
+	public static double search (String productName , int quantity){
+		double price = 0.0;
+		System.out.println("invoked search: productName (string) quantity"); 
+		if (productName=="menShirt"){
+		price=400.0*quantity;
+		}
+	   if (productName=="menGreenShirt"){
+		   price=400.0*quantity;
+		   }
+	   if  (productName=="menPinkShirt"){
+		   price=500.0*quantity;
+		   }
+		
+		if (productName=="menblackShirt"){
+		    price=400.0*quantity;
+		}
+		if (productName=="menWhiteShirt"){
+		price=400.0*quantity;
+		}		
+		if (productName=="menpant"){
+		price=400.0*quantity;
+		}
+	   if (productName=="menGreenPant"){
+		   price=400.0*quantity;
+		   }
+	   if  (productName=="menPinkPant"){
+		   price=400.0*quantity;
+		}
+	   
+	   if (productName=="menblackPant"){
+			price=400.0*quantity;
+        }
+		if (productName=="menWhitePant"){
+		price=400.0*quantity;
+		}		
+			if (productName=="menshorts"){
+		price=400.0*quantity;
+		 }
+	   if (productName=="menGreenShorts"){
+		   price=400.0*quantity;
+		   }
+	   if  (productName=="menPinkShorts"){
+		   price=400.0*quantity;
+		   }
+	   
+	   if (productName=="menblackShorts"){
+			price=400.0*quantity;
+			}
+		if (productName=="menWhiteShorts"){
+		price=400.0*quantity;
+		}		
+		if (productName=="menshoes"){
+			price=400.0*quantity;
+		}
+		if (productName=="menCrocs"){
+			price=400.0*quantity;
+			}
+		if (productName=="menRunningShoes"){
+			price=400.0*quantity;
+		}
+		if (productName=="menSliders"){
+			price=400.0*quantity;
+        }
+		if (productName=="menslippers"){
+			price=400.0*quantity;
+			}
+	   if (productName=="womenShirt"){
+		price=400.0*quantity;
+	   }
+	
+	  
+	   if (productName=="womenGreenShirt"){
+		   price=400.0*quantity;
+		
+	   }
+	   if (productName=="womenPinkShirt"){
+		   price=400.0*quantity;
+	   }
+		
+		if (productName=="womenblackShirt"){
+			price=400.0*quantity;
+	
+		}
+		if (productName=="womenWhiteShirt"){
+		price=400.0*quantity;
+
+		}		
+		if (productName=="womenPant"){
+		price=400.0*quantity;
+	
+	   }
+	   if (productName=="womenGreenPant"){
+		   price=400.0*quantity;
+		
+	   }
+	   if  (productName=="womenPinkPant"){
+		   price=400.0*quantity;
+	
+	   }
+	   
+	   if (productName=="womenblackPant"){
+	    price=400.0*quantity;
+	
+		}
+		if (productName=="womenWhitePant"){
+		price=400.0*quantity;
+
+		}		
+			if (productName=="womenshorts"){
+		price=400.0*quantity;
+
+	   }
+	   if (productName=="womenGreenShorts"){
+		   price=400.0*quantity;
+	
+	   }
+	   if  (productName=="womenPinkShorts"){
+		   price=400.0*quantity;
+		
+	   }
+	   
+	   if (productName=="womenblackShorts"){
+			price=400.0*quantity;
+		
+		}
+		if (productName=="womenWhiteShorts"){
+		price=400.0*quantity;
+
+		}		
+		if (productName=="womenshoes"){
+			price=400.0*quantity;
+
+		}
+		if (productName=="womenCrocs"){
+			price=400.0*quantity;
+
+		}
+		if (productName=="womenRunningShoes"){
+			price=400.0*quantity;
+		
+		}
+		if (productName=="womenSliders"){
+			price=400.0*quantity;
+		
+		}
+		if (productName=="womenslippers"){
+		price=400.0*quantity;
+		}
+		System.out.println("search ended");
+		
+		return price;
+	   
+	}
+}
